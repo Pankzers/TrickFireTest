8ef58aca36b89c65f9c054bde092216fbebb8d51
Connor Browne <Pankzers@gmail.com>
2018-09-11 14:06:26 -0700

    Screwing over Connor. Added a line to force a conflict.

--- ForConflict.cpp
+++ ForConflict.cpp
@@ -7,6 +7,7 @@ void main()
 	//I have no idea why this is happening.
 	cout << "What is going on." << endl;}
 	cout << "Double Commit Oh noes!" << endl;
-        cout << "Java is the superior language" << endl;
+	cout << "Java is the superior language" << endl;
+	cout << "HAHA!" << endl;
 }
 
