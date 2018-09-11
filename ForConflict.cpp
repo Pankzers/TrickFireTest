ac4fe28e38d8881f22efa6a12aaa34164764fb18
Ji <jkang18@uw.edu>
2018-09-11 13:54:55 -0700

    LIES

--- ForConflict.cpp
+++ ForConflict.cpp
@@ -4,11 +4,6 @@ using namespace std;
 void main()
 {
 	cout << "The original text." << endl;
-<<<<<<< HEAD
-    cout << "Java is the superior language" << endl; 
-}
-=======
-	cout << "Double Commit Oh noes!" << endl;
 
+    cout << "Java is the superior language" << endl;
 }
->>>>>>> f9462528686dbcec19aaaba168a213e2452ae50a
